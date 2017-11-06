fa786165af5aa63b3f1bd928927a8da20ce954a4
solufemi91 <stevenoo@spartaglobal.com>
2017-11-06 21:19:07 +0000

    added a moving keeper plus a penalty box

---
+++ main.js
@@ -0,0 +1,42 @@
+$(function(event) {
+
+var $goalkeeper = $('#goalkeeper')
+
+
+rightMove();
+
+function rightMove() {
+
+  var pos = -13;
+  var id = setInterval(frame, 5);
+  var id2 = 0
+  function frame() {
+    if (pos == 323) {
+      clearInterval(id);
+      var id2 = setInterval(leftMove, 5)
+    } else {
+      pos++;
+//
+      $goalkeeper.css("margin-left", pos + 'px');
+    }
+  }
+  function leftMove() {
+    if (pos == -13){
+      clearInterval(id2);
+    } else {
+    pos--;
+    $goalkeeper.css("margin-left", pos + 'px');
+  }
+}
+
+
+}
+
+
+
+
+
+
+
+
+})
