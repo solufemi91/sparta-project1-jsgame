27c637536abf1225b4945d128810dbbcda29675a
solufemi91 <stevenoo@spartaglobal.com>
2017-11-07 14:03:11 +0000

    keeper moves in a fixed loop

--- main.js
+++ main.js
@@ -6,21 +6,34 @@ var $shootButton = $('button');
 var $keeperScore = $('#keeperScore')
 var $strikerScore = $('#strikerScore')
 var score = 0
+var ballDistance = 0;
 
 //EVENT LISTENERS
-$shootButton.click(function(){
-  kickBall();
-})
+// $shootButton.click(function(){
+//   kickBall();
+// })
 
 $footballImage.click(function(){
   kickBall();
 })
 
+
 $goalkeeper.click(function(){
-  rightMove();
+  var i = 0;
+  while (i < 5) {
+    moveKeeper();
+    i++;
+
+  }
+
+});
 
 
-})
+function moveKeeper(){
+  $goalkeeper.animate({marginLeft: "323px"});
+  $goalkeeper.animate({marginLeft:"-13px"})
+}
+
 
 function kickBall(){
   var ballDistance = 16;
@@ -41,37 +54,37 @@ function kickBall(){
 }
 
 /// GOALKEEPER MOVING SIDE TO SIDE
-
-function rightMove() {
-
-  var pos = -13;
-  var id = setInterval(frame, 5);
-  var id2 = 0
-
-  function frame() {
-    if (pos == 323) {
-      clearInterval(id);
-      var id2 = setInterval(leftMove, 5)
-    } else {
-      pos++;
 //
-      $goalkeeper.css("margin-left", pos + 'px');
-    }
-  }
-  function leftMove() {
-    if (pos == -13){
-      clearInterval(id2);
-      setInterval(frame, 5)
-
-
-    } else {
-    pos--;
-    $goalkeeper.css("margin-left", pos + 'px');
-  }
-}
-
-
-}
+// function rightMove() {
+//
+//   var pos = -13;
+//   var id = setInterval(frame, 5);
+//   var id2 = 0
+//
+//   function frame() {
+//     if (pos == 323) {
+//       clearInterval(id);
+//       var id2 = setInterval(leftMove, 5)
+//     } else {
+//       pos++;
+// //
+//       $goalkeeper.css("margin-left", pos + 'px');
+//     }
+//   }
+//   function leftMove() {
+//     if (pos == -13){
+//       clearInterval(id2);
+//       setInterval(frame, 5)
+//
+//
+//     } else {
+//     pos--;
+//     $goalkeeper.css("margin-left", pos + 'px');
+//   }
+// }
+//
+//
+// }
 
 
 
